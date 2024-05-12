21c396c06041dafdc6f854c0366cea12ba96eebf
Ankit <rankit2883@gmail.com>
2024-05-12 14:27:11 +0530

    proxy handled

--- backend/server.js
+++ backend/server.js
@@ -20,10 +20,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
-app.use(cors({
-	origin: 'http://localhost:3000',  // Allow requests from your frontend origin
-	credentials: true,
-}))
+app.use(cors())
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chatapp-backend-1-troo.onrender.com", {
+			const socket = io("https://chatapp-backend-fz9q.onrender.com/", {
 				query: {
 					userId: authUser._id,
 				},
