0b0dc25f8c70e2598d44c61348803bab62cf4766
Ankit <rankit2883@gmail.com>
2024-05-12 14:05:46 +0530

    proxy changed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
 		port: 3000,
 		proxy: {
 			"/api": {
-				target: "https://chatapp-backend-1-troo.onrender.com/",
+				target: "http://localhost:4040",
 			},
 		},
 	},
