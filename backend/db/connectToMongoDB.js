3b0005b4877931e1b6d3adf4b03216f57f919eda
Ankit kumar <73269179+ankit-lang@users.noreply.github.com>
2025-03-16 12:29:43 +0000

    for vercel

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
 	try {
-		await mongoose.connect(process.env.MONGO_DB_URI);
+		await mongoose.connect(process.env.MONGO_DB_URI || "mongodb+srv://rankit2883:Ankit%409211@cluster0.qkaehdg.mongodb.net/chats");
 		console.log("Connected to MongoDB");
 	} catch (error) {
 		console.log("Error connecting to MongoDB", error.message);
